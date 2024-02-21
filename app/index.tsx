2ad4cd3ec3dbe39f280c9a9490936b02c00bb5b0
jatobro <jakob.tossebro@gmail.com>
2024-02-21 19:03:36 +0100

    make sure location is permitted

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
 import * as Location from "expo-location";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 
 import { AlertButton } from "../components/AlertButton";
 import { MenuButton } from "../components/MenuButton";
 
 const Index = () => {
+  const [granted, setGranted] = useState(false);
+
   useEffect(() => {
     (async () => {
       const { status } = await Location.requestBackgroundPermissionsAsync();
 
       if (status === "granted") {
         console.log("location permission success");
+        setGranted(true);
       } else {
         console.error("location permission denied");
       }
@@ -22,7 +25,9 @@ const Index = () => {
     <View className="flex h-full justify-around">
       <View />
       <View className="flex flex-col items-center justify-center">
-        <Text className="mb-3 text-lg">Trykk for å få hjelp</Text>
+        <Text className="mb-3 text-lg">
+          {granted ? "Trykk for å få hjelp" : "TILLAT STEDSTJENESTER"}
+        </Text>
         <AlertButton />
       </View>
       <View className="flex h-1/3 w-full flex-row items-center justify-around p-4">

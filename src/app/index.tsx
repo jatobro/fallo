1306a1e832ebb4af34c9ffa6d7bbbc434c61f5b2
jatobro <jakob.tossebro@gmail.com>
2024-03-13 11:14:50 +0100

    wait for async storage to resolve

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,21 @@
 import { useEffect } from "react";
 import { Redirect } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { listenAlerts } from "~/api/alerts";
 import { requestLocationPermission } from "~/services/location";
+import { User } from "~/types";
 
 const Page = () => {
-  useEffect(() => {
+  const onAppStart = async () => {
+    await requestLocationPermission();
+    const user = (await AsyncStorage.getItem("user")) as User;
+    console.log("user: ", user);
     listenAlerts();
-    requestLocationPermission();
+  };
+
+  useEffect(() => {
+    onAppStart();
   }, []);
 
   return <Redirect href="/home" />;

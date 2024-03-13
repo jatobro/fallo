77d6384296846be625237fe8ebc19459f2558662
jatobro <jakob.tossebro@gmail.com>
2024-03-13 10:15:14 +0100

    explicit initial value

--- src/atoms.ts
+++ src/atoms.ts
@@ -4,5 +4,6 @@ import { atomWithStorage, createJSONStorage } from "jotai/utils";
 import { User } from "./types";
 
 const storage = createJSONStorage<User | null>(() => AsyncStorage);
+const content = null;
 
-export const userAtom = atomWithStorage<User | null>("user", null, storage);
+export const userAtom = atomWithStorage<User | null>("user", content, storage);
